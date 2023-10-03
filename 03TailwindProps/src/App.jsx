2d439b8bcbcb8a8c247cbc713fac5f96005829fe
Fahim047 <fahimulislam58@gmail.com>
2023-10-03 22:59:22 +0600

    fix: tailwind setup and props

--- 03TailwindProps/src/App.jsx
+++ 03TailwindProps/src/App.jsx
@@ -6,11 +6,11 @@ function App() {
     "https://images.pexels.com/photos/1537671/pexels-photo-1537671.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   ]
   return (
-    <Fragment className='flex'>
+    <>
       <h1 className='bg-red-900 rounded p-5 border cursor-pointer text-center text-white'>Tailwind CSS</h1>
       <Item name="Bag" stock="stock out" price="$150" image={imageArray[0]}/>
       <Item name="Shoe" stock="stock out" price="$120" image={imageArray[1]}/>
-    </Fragment>
+    </>
   )
 }
 
